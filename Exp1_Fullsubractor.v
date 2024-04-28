8a9af1e86bed703d1adcd0777cf6dfdaef88a1a7
Sachita02 <162723490+Sachita02@users.noreply.github.com>
2024-04-28 13:09:14 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,17 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
-output d,bout;
+module fa_ha(a,b,c,sum,carry);
+input a,b,c;
+output sum,carry;
 
 
 
+
+module fa (a,b,c,sum, carry); 
+input a,b,c; 
+output sum, carry; 
+wire w1,w2,w3; 
+xor gl(wl,a,b); 
+xor g2 (w2,a,b); 
+xor g3 (sum, w1,c); 
+and (w3,c,w1); 
+or g5 (carry, w3,w2); 
 endmodule
